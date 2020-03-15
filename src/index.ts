5bd3ecb1d43470068b03c8ae952f71f8e19475b8
Jonathan Glanz <jonathan.glanz@gmail.com>
2020-03-15 15:17:45 -0400

    fixed truthy

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,66 @@
 
-/**
- * Export all functions from ./log
- */
-
-
-import { configure } from "./Config"
-
-export * from './Config'
 
 /**
- * Export all required types
+ * Log level values
  */
-export * from "./Types"
+export enum Level {
+  trace = "trace",
+  debug = "debug",
+  info = "info",
+  warn = "warn",
+  error = "error",
+  fatal = "fatal"
+}
+
+export type LevelName = keyof typeof Level
+
+export const LevelNames:Array<LevelName> = Object.values(Level)
+
+export type ILogger = {
+  [Level in LevelName]: (message: string, ...args:any[]) => void
+}
+
+export type LoggingProvider<ExtraArgs extends any[] = []> =  (name: string, ...extraArgs:ExtraArgs) => ILogger
+
+export const DefaultLoggingProvider: LoggingProvider = (name:string) => {
+  
+  /**
+   * Creates a simple logger, parsing
+   * provided category as a simple filename
+   * and using the current output for output
+   *
+   * @param name
+   * @returns {ILogger}
+   */
+  
+    name = name.split("/").pop().split(".").shift()
+    
+    
+    // Create levels
+    return LevelNames.reduce((logger:any, level) => ({
+      ...logger,
+      [level]: (...args) => {
+        let fn = console[level]
+        fn = typeof fn === "function" ? fn.bind(console) : console.log.bind(console)
+        fn(`[${name}] (${level})`,...args)
+      }
+    }), { name } as any) as ILogger
+    
+    
+    
+  
+}
+
+const loggerState = {
+  loggingProvider: DefaultLoggingProvider as LoggingProvider<any>
+}
+
+export function setLoggingProvider(newLoggingProvider: LoggingProvider) {
+  loggerState.loggingProvider = newLoggingProvider
+}
+
+export function getLogger<ExtraArgs extends any[] = []>(name: string, ...args: ExtraArgs) {
+  return (loggerState.loggingProvider as LoggingProvider<ExtraArgs>)(name,...args)
+}
 
 
-
-/**
- * By default export Log manager
- */
-// import { getLogger } from "./Logger"
-export default configure
