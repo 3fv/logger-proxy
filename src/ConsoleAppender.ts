f13cad86d5f0b75ed4f4d9c90f2d836edd9e4a3e
Jonathan Glanz <jonathan.glanz@gmail.com>
2021-04-24 20:28:12 -0400

    wip

--- src/ConsoleAppender.ts
+++ src/ConsoleAppender.ts
@@ -1,6 +1,5 @@
 import { LevelKind, Appender, LogRecord } from "./types"
 import { asOption } from "@3fv/prelude-ts"
-import { isObject, isString } from "@3fv/guard"
 
 
 const consoleLogBindings = new Map<LevelKind, (...args: any[]) => any>()
@@ -9,6 +8,12 @@ function getBoundConsoleFn(level: LevelKind) {
   return (console[level] ?? console.log).bind(console)
 }
 
+/**
+ * Get a cached console bound log function
+ *
+ * @param {LevelKind} level
+ * @returns {(...args: any[]) => any}
+ */
 export function getConsoleLogBinding(level: LevelKind) {
   if (!consoleLogBindings.has(level)) {
     consoleLogBindings.set(level, getBoundConsoleFn(level))
@@ -17,20 +22,34 @@ export function getConsoleLogBinding(level: LevelKind) {
   return consoleLogBindings.get(level)
 }
 
+/**
+ * ConsoleAppender Configuration
+ */
 export interface ConsoleAppenderConfig<Record extends LogRecord = any> {
   cacheEnabled: boolean
   prettyPrint: boolean
 }
 
+/**
+ * Partial of config, used for shortening instead of Partial<...>
+ */
 export type ConsoleAppenderOptions<Record extends LogRecord> = Partial<
   ConsoleAppenderConfig<Record>
 >
 
+/**
+ * Default console config
+ * @type {ConsoleAppenderConfig}
+ */
 const defaultConfig: ConsoleAppenderConfig = {
   cacheEnabled: true,
   prettyPrint: true
 }
 
+/**
+ * Console appender, the simple default appender used
+ * everywhere OOB
+ */
 export class ConsoleAppender<Record extends LogRecord>
   implements Appender<Record> {
  
